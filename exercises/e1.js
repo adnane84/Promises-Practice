541f7613a41b4dfbc67c5561636f7f0481d61c82
Adnane Abid <92402125+adnane84@users.noreply.github.com>
2023-09-22 12:06:21 -0400

    solving exercises

--- exercises/e1.js
+++ exercises/e1.js
@@ -16,6 +16,17 @@
 
 // Your code goes here
 
+export const getPromise = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("The PROMISE was RESOLVED");
+  }, 1000)
+});
+
+getPromise.then((resolveValue) => {
+  console.log(resolveValue);
+  return resolveValue;
+});
+
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-1"
 // If the test has all tests passed, switch to the next exercise file
